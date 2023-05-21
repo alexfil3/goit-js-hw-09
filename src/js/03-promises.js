c9b2695f6434a99d102daee5c8733cb1abad0bd9
Alex Filimonov <filimonovalexander9997@gmail.com>
2023-05-21 11:52:39 +0300

    delete additional comments

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -20,11 +20,9 @@ form.addEventListener('submit', (e) => {
     createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
     
       position += 1;
